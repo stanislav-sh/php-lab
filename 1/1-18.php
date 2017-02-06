8a683addc0e9dabf080aecc20a94a02e9e5897e3
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-06 17:11:00 +0300

    1-18

---
+++ 1/1-18.php
@@ -0,0 +1,22 @@
+<?php
+function digitsSum($x){
+    $sum = 0;
+    for ($j = 1; $j <= $x / 2; $j++) {
+        if ($x % $j == 0) {
+            $sum += $j;
+        }
+    }
+    return $sum;
+}
+$n = 1;
+$m = 2000;
+
+for ($i = $n; $i <= $m; $i++) {
+
+    $j = digitsSum($i);
+    if($i < $j && digitsSum($j) == $i){
+        echo  $i.' '.$j.'<br>';
+    }
+
+
+}
