fb7e7548c6bdf5b4e4427ce581cd0ba1c23dffa6
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-06 17:38:12 +0300

    1-22

---
+++ 1/1-22.php
@@ -0,0 +1,19 @@
+<?
+function is_prime($n)
+{
+    if ($n == 1)
+        return false;
+    for ($i = 2; $i * $i <= $n; $i++) {
+        if ($n % $i == 0)
+            return false;
+    }
+    return true;
+}
+
+$n = 1037;
+$count = 0;
+for ($i = 1; $i <= $n / 2; $i++) {
+    if ($n % $i == 0 && is_prime($i)) {
+        echo $i . ' ';
+    }
+}
