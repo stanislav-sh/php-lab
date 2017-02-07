7a2dfa250b684f5dc731e12d06d634613362b78f
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-07 16:49:09 +0300

    3-15

---
+++ 3/3-15.php
@@ -0,0 +1,17 @@
+<?php
+$A = Array(1, 14, -54, 67, 12, 0, 34, 5, -27, 54, 3);
+$k = 3;
+$n = 39;
+$i = 0;
+while(isset($A[$i + $k])){
+    $sum = 0;
+    for($j = $i; $j < $i + $k; $j++){
+        $sum += $A[$j];
+    }
+    if($sum == $n){
+        echo "Sequence starting from index ".$i;
+        exit;
+    }
+    $i++;
+}
+echo "There is no such sequence";
