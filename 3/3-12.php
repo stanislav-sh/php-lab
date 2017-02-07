557ac10f7f0c590f05abc3009787584bf265c4c4
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-07 16:25:05 +0300

    3-12

--- 3/3-12.php
+++ 3/3-12.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 07.02.2017
- * Time: 14:55
- */
+$A = Array(1, 14, 54, -67, -12, 0, 34, 5, 27, 54, 3);
+$i = 0;
+while (isset($A[$i])) {
+    if(!isset($min) && $A[$i] <0){
+        $min = $A[$i];
+    }
+    if(!isset($max) && $A[$i] >0){
+        $max = $A[$i];
+    }
+    if (isset($min) && $A[$i] < $min && $A[$i] > 0) {
+        $min = $A[$i];
+    }
+    if(isset($max) && $A[$i] > $max && $A[$i] < 0){
+        $max = $A[$i];
+    }
+    $i++;
+}
+
+echo isset($max)? 'Min positive: '.$max.'<br>' : 'Min positive not found<br>';
+echo isset($min)? 'Max negative:'.$min : 'Min positive not found';
