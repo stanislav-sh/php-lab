71aae022e34f833736754db6a472f3de045df280
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-07 15:07:55 +0300

    3-4

--- 3/3-4.php
+++ 3/3-4.php
@@ -1,6 +1,10 @@
 <?php
-$A = Array(1,1, 14, -54, 67,67, 12, 0, 34, 5, -27, 54, 3,3);
+$A = Array(1,1,14, -54, 67, 12, 0, 34, 5, -27, 54,54,3,3, 3);
 $i = 1;
+if(!isset($A[$i])){
+    echo 'Array is too small';
+    exit;
+}
 $count = 0;
 while (isset($A[$i])) {
     if ($A[$i] == $A[$i-1]) {
