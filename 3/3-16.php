57d972933d89ff700647c7af4fe17600e64c91a5
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-07 16:56:21 +0300

    3-16

---
+++ 3/3-16.php
@@ -0,0 +1,21 @@
+<?php
+$A = Array(1,1,1, 14, -54, 67, 12,12,12,12, 0, 34, 5, -27, 54, 3);
+$maxlen = 0;
+$i = 0;
+while(isset($A[$i])){
+    $len = 1;
+    while(isset($A[$i + 1])){
+        if($A[$i] == $A[$i+1]){
+            $len++;
+            $i++;
+        }
+        else{
+            break;
+        }
+    }
+    if($len > $maxlen){
+        $maxlen = $len;
+    }
+    $i++;
+}
+echo $maxlen;
