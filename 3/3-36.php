1dc158923a8bbe283b29da2b79474c85e4dbf435
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-09 15:59:27 +0300

    3-32

--- 3/3-36.php
+++ 3/3-36.php
@@ -1,19 +1,23 @@
 <?php
-$arr = Array(0, 0, 1, 1, 2, 2,-1,15,-40, 0, 1, 2,2, 1,0);
+$arr = Array(1,0, 10, 31, 21, 42, 2,-1,15,-40, 0, 1, 2,2, 1,0,-1);
 $size = count($arr);
-for ($i = 0; $i < $size; $i++) {
-    for ($j = 0; $j < $size - 1 - $i; $j++) {
-        if ($arr[$j + 1] < $arr[$j]) {
+for ($i = $size -1; $i >=0 ; $i--) {
+    for ($j = $size - 2 - $i; $j >= 0; $j--) {
+        if ($arr[$j] > 0) {
             $tmp = $arr[$j];
             $arr[$j] = $arr[$j + 1];
             $arr[$j + 1] = $tmp;
         }
     }
 }
+$k = 0;
+while($arr[$k] <= 0){
+    $k++;
+}
 
-for ($i = 0; $i < $size; $i++) {
-    for ($j = 0; $j < $size - 1 - $i; $j++) {
-        if ($arr[$j + 1] == 0) {
+for ($i = $k; $i < $size; $i++) {
+    for ($j = $k; $j < $size - 1; $j++) {
+        if ($arr[$j + 1] > $arr[$j]) {
             $tmp = $arr[$j];
             $arr[$j] = $arr[$j + 1];
             $arr[$j + 1] = $tmp;
---
+++ 3/3-40.php
@@ -0,0 +1,48 @@
+<?php
+function count_arr($x)
+{
+    $count = 0;
+    while (isset($x[$count])) {
+        $count++;
+    }
+    return $count;
+}
+$A = Array(1,2,4,9,12,24,56);
+$B = Array(-12,0,2,6,90);
+$C = Array();
+$i = 0;
+$j = 0;
+$ALen = count_arr($A);
+$BLen = count_arr($B);
+$index = 0;
+    while ($i < $ALen && $j < $BLen)
+    {
+        if ($A[$i] < $B[$j])
+        {
+            $C[$index] = $A[$i];
+            $i++;
+        }
+        else
+        {
+            $C[$index] = $B[$j];
+            $j++;
+        }
+
+        $index++;
+    }
+
+    while ($i < $ALen)
+    {
+        $C[$index] = $A[$i];
+        $index++;
+        $i++;
+    }
+
+    while ($j < $BLen)
+    {
+        $C[$index] = $B[$j];
+        $index++;
+        $j++;
+    }
+
+print_r($C);
