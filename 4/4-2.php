b39fbbe9094c457f0ab90d5117c47d4087b774f8
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-09 16:25:56 +0300

    4-2

---
+++ 4/4-2.php
@@ -0,0 +1,26 @@
+<?php
+$A = Array(
+    Array(1, 2, 3),
+    Array(4, 5, 6),
+    Array(0, 1, 2));
+$i = 0;
+
+while (isset($A[$i])) {
+    $sumRow = 0;
+    $j = 0;
+    while (isset($A[$i][$j])) {
+        $sumRow += $A[$i][$j];
+        $j++;
+    }
+    if (!isset($min)) {
+        $min = $sumRow / $j;
+        $minNdx = $i;
+    } else {
+        if (($sumRow / $j) < $min) {
+            $min = ($sumRow / $j);
+            $minNdx = $i;
+        }
+    }
+    $i++;
+}
+echo 'Row number: '.($minNdx + 1);
