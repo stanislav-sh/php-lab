aa1c50f44aac8def7faf49f2ad399a14b7253fbf
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-09 13:24:39 +0300

    3-12

--- 3/3-12.php
+++ 3/3-12.php
@@ -1,21 +1,21 @@
 <?php
-$A = Array(1, 14, 54, -67, -12, 0, 34, 5, 27, 54, 3);
+$A = Array(100, 14, 54, -67, -12, 0, 34, -5, 27, 54, 3);
 $i = 0;
 while (isset($A[$i])) {
-    if (!isset($min) && $A[$i] < 0) {
-        $min = $A[$i];
-    }
-    if (!isset($max) && $A[$i] > 0) {
+    if (!isset($max) && $A[$i] < 0) {
         $max = $A[$i];
     }
-    if (isset($min) && $A[$i] < $min && $A[$i] > 0) {
+    if (!isset($min) && $A[$i] > 0) {
         $min = $A[$i];
     }
     if (isset($max) && $A[$i] > $max && $A[$i] < 0) {
         $max = $A[$i];
     }
+    if (isset($min) && $A[$i] < $min && $A[$i] > 0) {
+        $min = $A[$i];
+    }
     $i++;
 }
 
-echo isset($max) ? 'Min positive: ' . $max . '<br>' : 'Min positive not found<br>';
-echo isset($min) ? 'Max negative:' . $min : 'Min positive not found';
+echo isset($min) ? 'Min positive: ' . $min . '<br>' : 'Min positive not found<br>';
+echo isset($max) ? 'Max negative:' . $max : 'Max negative not found';
