e8f18ff3d57c7577d61b6981e4c7c804890a649b
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-09 17:54:53 +0300

    4-10

--- 4/4-10.php
+++ 4/4-10.php
@@ -1,7 +1,47 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 09.02.2017
- * Time: 16:38
- */
+function count_arr($x)
+{
+    $count = 0;
+    while (isset($x[$count])) {
+        $count++;
+    }
+    return $count;
+}
+function delete_from_array($A,$index){
+    while($index < count_arr($A)-1){
+        $A[$index] = $A[$index+1];
+        $index++;
+    }
+    unset($A[$index]);
+    return $A;
+}
+function findMax($x)
+{
+    $i = 0;
+    $max = $x[0];
+    while (isset($x[$i])) {
+        if ($x[$i] > $max) {
+            $max = $x[$i];
+        }
+        $i++;
+    }
+    return $max;
+}
+$A = Array(
+    Array(3, -2, -3,3),
+    Array(-27, -81, 8310,1),
+    Array(8320,81, -1, 8320));
+$i = 0;
+while (isset($A[$i])) {
+    $j = 0;
+    $max = findMax($A[$i]);
+    while (isset($A[$i][$j])) {
+        if ($A[$i][$j] == $max) {
+            $A[$i] = delete_from_array($A[$i],$j);
+            $j--;
+        }
+        $j++;
+    }
+    $i++;
+}
+print_r($A);
