5575c061b8feaf0d53a59e274f48b40f1f629480
stabislav-sh <stanislav-sh@phpdev.org>
2017-02-10 09:15:26 +0300

    3-26

--- 3/3-26.php
+++ 3/3-26.php
@@ -1,5 +1,14 @@
 <?php
-$A = Array(1, 14, -54, 67, 12, 0, 34, 5, -27, 54, 3);
+function count_arr($x)
+{
+    $count = 0;
+    while (isset($x[$count])) {
+        $count++;
+    }
+    return $count;
+}
+
+$A = Array(1, 14, -54, 67, 12, 0, 34, 5, -27, -54, 3);
 $i = 0;
 $max = $A[$i];
 while (isset($A[$i])) {
@@ -11,13 +20,12 @@ while (isset($A[$i])) {
 $B = Array();
 $j = 0;
 $i = 0;
-while(isset($A[$i])){
+for ($i = 0; $i < count_arr($A); $i++) {
     $B[$j] = $A[$i];
-    if($A[$i] % 2 == 0){
+    if ($A[$i] % 2 == 0 && $A[$i] < 0) {
         $j++;
         $B[$j] = $max;
     }
-    $i++;
     $j++;
 }
 print_r($B);
